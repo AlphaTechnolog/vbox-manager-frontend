be61886ae4b65978b818ab53525d5790902fe774
Gabriel Guerra <gfranklings@gmail.com>
2024-10-22 12:32:16 -0400

    feat: VNC integration

--- src/views/VmDetail.vue
+++ src/views/VmDetail.vue
@@ -1,16 +1,29 @@
 <script setup lang="ts">
+// @ts-expect-error idk this types definition doesn't works
+import RFB from "@novnc/novnc/core/rfb";
 import { storeToRefs } from 'pinia';
-import { useAppState, useVms, type VM } from '@/stores';
+import { useAppState, useVms, VMStatus, type VM } from '@/stores';
 import { useRoute } from 'vue-router';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
+import { PhSmileyWink } from "@phosphor-icons/vue";
 
 const appState = useAppState();
 const vmsStore = useVms();
 
-const { selectedVM } = storeToRefs(appState);
+const { selectedVM, vmStatus } = storeToRefs(appState);
 const { vms } = storeToRefs(vmsStore);
 
+const wssUrl = computed(() => {
+  return `ws://${import.meta.env.VITE_SERVER_IP}:10001`;
+});
+
 const route = useRoute();
+
+const handleOnRequiredCredentials = (rfb?: RFB) => {
+  rfb.sendCredentials({
+    password: prompt("Enter password"),
+  });
+}
 
 onMounted(() => {
   if (vms.value.length > 0) {
@@ -22,5 +35,33 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>Selected vm is: {{ selectedVM?.name }}</h1>
+  <main class="flex flex-col gap-y-2 justify-center items-center h-[80%]">
+    <div v-if="vmStatus == VMStatus.stopped" class="text-dimmed_foreground flex flex-col justify-center items-center">
+      <ph-smiley-wink size="150px" />
+      <h3 class="text-xl">
+        This
+        <span class="text-blue">
+          vm
+        </span>
+        seems to be
+        <span class="text-red">
+          stopped
+        </span>
+      </h3>
+    </div>
+
+    <div class="flex flex-col justify-center items-center text-dimmed_foreground" v-else>
+      <vue-vnc
+        :url="wssUrl"
+        :onCredentialsRequired="handleOnRequiredCredentials"
+        focusOnClick
+      />
+      <p>
+        Connecting to
+        <span class="text-green">
+          {{ wssUrl }}
+        </span>
+      </p>
+    </div>
+  </main>
 </template>
